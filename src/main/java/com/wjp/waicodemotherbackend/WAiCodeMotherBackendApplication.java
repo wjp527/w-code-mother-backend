9a35eb1f0a572b0aaac2dbc9f6b3701a6f1801c9
wjp <1060638479@qq.com>
2025-09-10 22:09:57 +0800

    1.初始化项目 2.添加依赖和配置（HuTooL和Knife4j接口文档） 3.提供基础代码（自定义异常、请求响应包装类） 4.提供基础接口（健康检查接口）

---
+++ src/main/java/com/wjp/waicodemotherbackend/WAiCodeMotherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wjp.waicodemotherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WAiCodeMotherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WAiCodeMotherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjp/waicodemotherbackend/WAiCodeMotherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjp.waicodemotherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WAiCodeMotherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
